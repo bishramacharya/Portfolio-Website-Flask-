9d4d3d44199e8fc52fcb1f65269a4346b632e8e5
Bishram Acharya <bisram019@gmail.com>
2022-12-08 22:05:43 +0545

    My Portfoilio Website

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def default():
+    return render_template('index.html')
+
+
+@app.route("/<string:page_name>")
+def page(page_name):
+    return render_template(page_name)
